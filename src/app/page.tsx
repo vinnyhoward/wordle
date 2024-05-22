6f7529a42e79422fdc8ae576b0915797615e0d22
Vincent Howard <vincenguyenhoward@gmail.com>
2024-05-21 19:20:51 -0600

    solved fe challenge - creating basic wordle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,227 @@
-import Image from "next/image";
+"use client";
+import { useState, useEffect } from "react";
+
+const dict = [
+  "apple",
+  "bread",
+  "dance",
+  "eagle",
+  "fruit",
+  "grass",
+  "heart",
+  "light",
+  "magic",
+  "curry",
+];
+
+const NUM_INPUTS = 5;
+
+const fetchWord = () => {
+  const randomIndex = Math.ceil(Math.random() * dict.length);
+  return dict[randomIndex];
+};
+
+const fillArr = Array.from({ length: NUM_INPUTS }).fill("");
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [word] = useState<string>(fetchWord());
+  const [row1, setRow1] = useState<any[]>(fillArr);
+  const [row2, setRow2] = useState<any[]>(fillArr);
+  const [row3, setRow3] = useState<any[]>(fillArr);
+  const [row4, setRow4] = useState<any[]>(fillArr);
+  const [row5, setRow5] = useState<any[]>(fillArr);
+  const [row6, setRow6] = useState<any[]>(fillArr);
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  const [isWinner, setWinner] = useState(false);
+  const [isLoser, setLoser] = useState(false);
+
+  const handleChange = (
+    event: any,
+    index: number,
+    setFunc: any,
+    state: any
+  ) => {
+    const value = event.target.value;
+    const re = /^[A-Za-z]+$/;
+    if (re.test(value)) {
+      const newArray = [...state];
+      newArray[index] = value.toLowerCase();
+      setFunc(newArray);
+    }
+  };
+
+  const getWordHashMap = () => {
+    const hash: any = {};
+    word.split("").forEach((letter) => {
+      if (!hash[letter]) hash[letter] = 1;
+      else hash[letter] += 1;
+    });
+    return hash;
+  };
+
+  const getStyles = (index: number, row: any) => {
+    const wordHashMap = getWordHashMap();
+    const correctPlace = "bg-emerald-400";
+    const incorrectPlace = "bg-amber-500";
+    const error = "bg-slate-500";
+    let styles = "";
+
+    const letter = row[index];
+    if (row[index] !== word[index] && wordHashMap[letter] > 0) {
+      wordHashMap[letter] -= 1;
+      styles = incorrectPlace;
+    } else if (row[index] === word[index]) {
+      styles = correctPlace;
+    } else if (letter !== "") {
+      styles = error;
+    }
+
+    return styles;
+  };
+
+  const renderInputList1 = () => {
+    return row1.map((letter: string, index: number) => {
+      const styles = getStyles(index, row1);
+      const isDisabled = row1.join("").length === 5;
+      return (
+        <input
+          disabled={isDisabled}
+          key={`${index}`}
+          className={`border-3 m-2 border-[5px] border-gray-400 h-[100px] 
+          w-[100px] text-[48px] p-[30px] rounded-xl text-white ${styles}`}
+          value={letter}
+          onChange={(event) => handleChange(event, index, setRow1, row1)}
+          maxLength={1}
+          type="text"
         />
+      );
+    });
+  };
+
+  const renderInputList2 = () => {
+    return row2.map((letter: string, index: number) => {
+      const styles = getStyles(index, row2);
+      const isDisabled = row2.join("").length === 5 || row1.join("").length < 5;
+      return (
+        <input
+          disabled={isDisabled}
+          key={`${index}`}
+          className={`border-3 m-2 border-[5px] border-gray-400 h-[100px] 
+          w-[100px] text-[48px] p-[30px] rounded-xl text-white ${styles}`}
+          value={letter}
+          onChange={(event) => handleChange(event, index, setRow2, row2)}
+          maxLength={1}
+          type="text"
+        />
+      );
+    });
+  };
+
+  const renderInputList3 = () => {
+    return row3.map((letter: string, index: number) => {
+      const styles = getStyles(index, row3);
+      const isDisabled = row3.join("").length === 5 || row2.join("").length < 5;
+      return (
+        <input
+          disabled={isDisabled}
+          key={`${index}`}
+          className={`border-3 m-2 border-[5px] border-gray-400 h-[100px] 
+          w-[100px] text-[48px] p-[30px] rounded-xl text-white ${styles}`}
+          value={letter}
+          onChange={(event) => handleChange(event, index, setRow3, row3)}
+          maxLength={1}
+          type="text"
+        />
+      );
+    });
+  };
+
+  const renderInputList4 = () => {
+    return row4.map((letter: string, index: number) => {
+      const styles = getStyles(index, row4);
+      const isDisabled = row4.join("").length === 5 || row3.join("").length < 5;
+      return (
+        <input
+          disabled={isDisabled}
+          key={`${index}`}
+          className={`border-3 m-2 border-[5px] border-gray-400 h-[100px] 
+          w-[100px] text-[48px] p-[30px] rounded-xl text-white ${styles}`}
+          value={letter}
+          onChange={(event) => handleChange(event, index, setRow4, row4)}
+          maxLength={1}
+          type="text"
+        />
+      );
+    });
+  };
+
+  const renderInputList5 = () => {
+    return row5.map((letter: string, index: number) => {
+      const styles = getStyles(index, row5);
+      const isDisabled = row5.join("").length === 5 || row4.join("").length < 5;
+      return (
+        <input
+          disabled={isDisabled}
+          key={`${index}`}
+          className={`border-3 m-2 border-[5px] border-gray-400 h-[100px] 
+          w-[100px] text-[48px] p-[30px] rounded-xl text-white ${styles}`}
+          value={letter}
+          onChange={(event) => handleChange(event, index, setRow5, row5)}
+          maxLength={1}
+          type="text"
+        />
+      );
+    });
+  };
+
+  const renderInputList6 = () => {
+    return row6.map((letter: string, index: number) => {
+      const styles = getStyles(index, row6);
+      const isDisabled = row6.join("").length === 5 || row5.join("").length < 5;
+      return (
+        <input
+          disabled={isDisabled}
+          key={`${index}`}
+          className={`border-3 m-2 border-[5px] border-gray-400 h-[100px] 
+          w-[100px] text-[48px] p-[30px] rounded-xl text-white ${styles}`}
+          value={letter}
+          onChange={(event) => handleChange(event, index, setRow6, row6)}
+          maxLength={1}
+          type="text"
+        />
+      );
+    });
+  };
+
+  useEffect(() => {
+    const word1 = row1.join("");
+    const word2 = row2.join("");
+    const word3 = row3.join("");
+    const word4 = row4.join("");
+    const word5 = row5.join("");
+    const word6 = row6.join("");
+    const wordArr = [word1, word2, word3, word4, word5, word6];
+    const isWinner = wordArr.filter((w: string) => w === word);
+    if (isWinner.length === 1) setWinner(true);
+    if (word6.length === 5 && word6 !== word) setLoser(true);
+  }, [row1, row2, row3, row4, row5, row6, isWinner, setWinner, word]);
+
+  console.log("word:", word);
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 mx-auto justify-center w-[750px]">
+      <div className="flex flex-col mb-4">
+        <div className="flex flex-row">{renderInputList1()}</div>
+        <div className="flex flex-row">{renderInputList2()}</div>
+        <div className="flex flex-row">{renderInputList3()}</div>
+        <div className="flex flex-row">{renderInputList4()}</div>
+        <div className="flex flex-row">{renderInputList5()}</div>
+        <div className="flex flex-row">{renderInputList6()}</div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="w-full h-[75px] flex items-center justify-center">
+        <span className="text-[18px] text-black">
+          {isWinner && "YOU WON BITCH"}
+          {isLoser && "YOU LOST BITCH"}
+        </span>
       </div>
     </main>
   );
