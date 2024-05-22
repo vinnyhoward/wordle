1dee1d71c68c7180636959176272d1dd2b9b07bc
Vincent Howard <vincenguyenhoward@gmail.com>
2024-05-21 21:13:41 -0600

    finished wordle fe challange

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const dict = [
   "apple",
@@ -14,63 +15,68 @@ const dict = [
   "curry",
 ];
 
-const NUM_INPUTS = 5;
-
 const fetchWord = () => {
-  const randomIndex = Math.ceil(Math.random() * dict.length);
+  const randomIndex = Math.ceil(Math.random() * dict.length + 1);
   return dict[randomIndex];
 };
 
-const fillArr = Array.from({ length: NUM_INPUTS }).fill("");
+const calculateFill = () => {
+  const numInputs = 5;
+  const numRows = 6;
+  const inputs = Array.from({ length: numInputs }).fill("");
+  const rows = Array.from({ length: numRows }).map(() => [...inputs]);
+  return rows;
+};
 
 export default function Home() {
-  const [word] = useState<string>(fetchWord());
-  const [row1, setRow1] = useState<any[]>(fillArr);
-  const [row2, setRow2] = useState<any[]>(fillArr);
-  const [row3, setRow3] = useState<any[]>(fillArr);
-  const [row4, setRow4] = useState<any[]>(fillArr);
-  const [row5, setRow5] = useState<any[]>(fillArr);
-  const [row6, setRow6] = useState<any[]>(fillArr);
-
+  const [word, setWord] = useState<string>(fetchWord());
+  const [guessesArray, setGuessesArray] = useState<any[]>(calculateFill());
+  const [currentRowIndex, setCurrentRowIndex] = useState(0);
+  const [currentColIndex, setCurrentColIndex] = useState(0);
   const [isWinner, setWinner] = useState(false);
   const [isLoser, setLoser] = useState(false);
 
-  const handleChange = (
-    event: any,
-    index: number,
-    setFunc: any,
-    state: any
-  ) => {
-    const value = event.target.value;
-    const re = /^[A-Za-z]+$/;
-    if (re.test(value)) {
-      const newArray = [...state];
-      newArray[index] = value.toLowerCase();
-      setFunc(newArray);
-    }
-  };
-
   const getWordHashMap = () => {
     const hash: any = {};
-    word.split("").forEach((letter) => {
-      if (!hash[letter]) hash[letter] = 1;
-      else hash[letter] += 1;
-    });
+    if (word && word.length) {
+      word.split("").forEach((letter) => {
+        if (!hash[letter]) hash[letter] = 1;
+        else hash[letter] += 1;
+      });
+    }
     return hash;
   };
 
-  const getStyles = (index: number, row: any) => {
-    const wordHashMap = getWordHashMap();
+  const wordHashMap = getWordHashMap();
+
+  const handleChange = (event: any, outerIndex: number, innerIndex: number) => {
+    const value = event.target.value;
+    const re = /^[A-Za-z]+$/;
+    if (re.test(value) && value !== " ") {
+      if (innerIndex === 4 && outerIndex !== 5) {
+        setCurrentRowIndex((currentRowIndex) => currentRowIndex + 1);
+        setCurrentColIndex(0);
+      } else {
+        setCurrentColIndex((prevColIndex) => prevColIndex + 1);
+      }
+
+      const newArray = [...guessesArray];
+      newArray[outerIndex][innerIndex] = value.toLowerCase();
+      setGuessesArray(newArray);
+    }
+  };
+
+  const getStyles = (outerIndex: number, innerIndex: number) => {
     const correctPlace = "bg-emerald-400";
     const incorrectPlace = "bg-amber-500";
     const error = "bg-slate-500";
     let styles = "";
 
-    const letter = row[index];
-    if (row[index] !== word[index] && wordHashMap[letter] > 0) {
+    const letter = guessesArray[outerIndex][innerIndex];
+    if (letter !== word[innerIndex] && wordHashMap[letter] > 0) {
       wordHashMap[letter] -= 1;
       styles = incorrectPlace;
-    } else if (row[index] === word[index]) {
+    } else if (letter === word[innerIndex]) {
       styles = correctPlace;
     } else if (letter !== "") {
       styles = error;
@@ -79,149 +85,81 @@ export default function Home() {
     return styles;
   };
 
-  const renderInputList1 = () => {
-    return row1.map((letter: string, index: number) => {
-      const styles = getStyles(index, row1);
-      const isDisabled = row1.join("").length === 5;
+  const renderInputList = () => {
+    return guessesArray.map((inputs: any[], outerIndex: number) => {
+      const isRowDisabled = outerIndex !== currentRowIndex;
       return (
-        <input
-          disabled={isDisabled}
-          key={`${index}`}
-          className={`border-3 m-2 border-[5px] border-gray-400 h-[100px] 
-          w-[100px] text-[48px] p-[30px] rounded-xl text-white ${styles}`}
-          value={letter}
-          onChange={(event) => handleChange(event, index, setRow1, row1)}
-          maxLength={1}
-          type="text"
-        />
+        <div key={uuidv4()} className="flex flex-row">
+          {inputs.map((letter, innerIndex) => {
+            const styles = getStyles(outerIndex, innerIndex);
+            const isDisabled = inputs.join("").length === 5;
+            return (
+              <input
+                disabled={
+                  isDisabled ||
+                  isRowDisabled ||
+                  letter !== "" ||
+                  isWinner ||
+                  isLoser ||
+                  currentColIndex !== innerIndex
+                }
+                key={uuidv4()}
+                className={`border-3 m-2 border-[5px] border-gray-700 h-[100px] 
+                w-[100px] text-[42px] p-[30px] rounded-xl text-white ${styles}`}
+                value={letter}
+                onChange={(event) =>
+                  handleChange(event, outerIndex, innerIndex)
+                }
+                maxLength={1}
+                type="text"
+              />
+            );
+          })}
+        </div>
       );
     });
   };
 
-  const renderInputList2 = () => {
-    return row2.map((letter: string, index: number) => {
-      const styles = getStyles(index, row2);
-      const isDisabled = row2.join("").length === 5 || row1.join("").length < 5;
-      return (
-        <input
-          disabled={isDisabled}
-          key={`${index}`}
-          className={`border-3 m-2 border-[5px] border-gray-400 h-[100px] 
-          w-[100px] text-[48px] p-[30px] rounded-xl text-white ${styles}`}
-          value={letter}
-          onChange={(event) => handleChange(event, index, setRow2, row2)}
-          maxLength={1}
-          type="text"
-        />
-      );
-    });
-  };
-
-  const renderInputList3 = () => {
-    return row3.map((letter: string, index: number) => {
-      const styles = getStyles(index, row3);
-      const isDisabled = row3.join("").length === 5 || row2.join("").length < 5;
-      return (
-        <input
-          disabled={isDisabled}
-          key={`${index}`}
-          className={`border-3 m-2 border-[5px] border-gray-400 h-[100px] 
-          w-[100px] text-[48px] p-[30px] rounded-xl text-white ${styles}`}
-          value={letter}
-          onChange={(event) => handleChange(event, index, setRow3, row3)}
-          maxLength={1}
-          type="text"
-        />
-      );
-    });
-  };
-
-  const renderInputList4 = () => {
-    return row4.map((letter: string, index: number) => {
-      const styles = getStyles(index, row4);
-      const isDisabled = row4.join("").length === 5 || row3.join("").length < 5;
-      return (
-        <input
-          disabled={isDisabled}
-          key={`${index}`}
-          className={`border-3 m-2 border-[5px] border-gray-400 h-[100px] 
-          w-[100px] text-[48px] p-[30px] rounded-xl text-white ${styles}`}
-          value={letter}
-          onChange={(event) => handleChange(event, index, setRow4, row4)}
-          maxLength={1}
-          type="text"
-        />
-      );
-    });
-  };
-
-  const renderInputList5 = () => {
-    return row5.map((letter: string, index: number) => {
-      const styles = getStyles(index, row5);
-      const isDisabled = row5.join("").length === 5 || row4.join("").length < 5;
-      return (
-        <input
-          disabled={isDisabled}
-          key={`${index}`}
-          className={`border-3 m-2 border-[5px] border-gray-400 h-[100px] 
-          w-[100px] text-[48px] p-[30px] rounded-xl text-white ${styles}`}
-          value={letter}
-          onChange={(event) => handleChange(event, index, setRow5, row5)}
-          maxLength={1}
-          type="text"
-        />
-      );
-    });
-  };
-
-  const renderInputList6 = () => {
-    return row6.map((letter: string, index: number) => {
-      const styles = getStyles(index, row6);
-      const isDisabled = row6.join("").length === 5 || row5.join("").length < 5;
-      return (
-        <input
-          disabled={isDisabled}
-          key={`${index}`}
-          className={`border-3 m-2 border-[5px] border-gray-400 h-[100px] 
-          w-[100px] text-[48px] p-[30px] rounded-xl text-white ${styles}`}
-          value={letter}
-          onChange={(event) => handleChange(event, index, setRow6, row6)}
-          maxLength={1}
-          type="text"
-        />
-      );
-    });
+  const onReset = () => {
+    setWinner(false);
+    setLoser(false);
+    setGuessesArray(calculateFill());
+    setWord(fetchWord());
+    setCurrentRowIndex(0);
   };
 
   useEffect(() => {
-    const word1 = row1.join("");
-    const word2 = row2.join("");
-    const word3 = row3.join("");
-    const word4 = row4.join("");
-    const word5 = row5.join("");
-    const word6 = row6.join("");
-    const wordArr = [word1, word2, word3, word4, word5, word6];
-    const isWinner = wordArr.filter((w: string) => w === word);
-    if (isWinner.length === 1) setWinner(true);
-    if (word6.length === 5 && word6 !== word) setLoser(true);
-  }, [row1, row2, row3, row4, row5, row6, isWinner, setWinner, word]);
+    const wordArr = guessesArray.map((guess: any) => {
+      if (guess) {
+        return guess.join("");
+      }
+      return guess;
+    });
 
-  console.log("word:", word);
+    const isWinner = wordArr.includes(word);
+    if (isWinner) {
+      return setWinner(true);
+    }
+
+    if (currentRowIndex === 5 && guessesArray[5].join("").length === 5) {
+      return setLoser(true);
+    }
+  }, [guessesArray, word, currentRowIndex]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 mx-auto justify-center w-[750px]">
-      <div className="flex flex-col mb-4">
-        <div className="flex flex-row">{renderInputList1()}</div>
-        <div className="flex flex-row">{renderInputList2()}</div>
-        <div className="flex flex-row">{renderInputList3()}</div>
-        <div className="flex flex-row">{renderInputList4()}</div>
-        <div className="flex flex-row">{renderInputList5()}</div>
-        <div className="flex flex-row">{renderInputList6()}</div>
-      </div>
-      <div className="w-full h-[75px] flex items-center justify-center">
-        <span className="text-[18px] text-black">
-          {isWinner && "YOU WON BITCH"}
-          {isLoser && "YOU LOST BITCH"}
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 mx-auto justify-center w-[750px] bg-gray-800">
+      <div className="flex flex-col mb-4">{renderInputList()}</div>
+      <div className="w-full h-[75px] flex items-center justify-center flex flex-col">
+        <span className="text-[18px] text-white my-3">
+          {isWinner && "YOU WON DUDE"}
+          {isLoser && "YOU LOST DUDE"}
         </span>
+        <button
+          onClick={onReset}
+          className="btn w-full h-[70px] bg-orange-500 border-none text-white text-[26px] hover:bg-orange-800"
+        >
+          Reset
+        </button>
       </div>
     </main>
   );
